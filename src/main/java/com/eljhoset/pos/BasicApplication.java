f559dd11943424c9a656ecf0626d8bab4fd15db3
eljhoset <jd-jd-@hotmail.com>
2018-08-22 21:45:11 -0400

    Project initial setup

---
+++ src/main/java/com/eljhoset/pos/BasicApplication.java
@@ -0,0 +1,12 @@
+package com.eljhoset.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicApplication.class, args);
+    }
+}
