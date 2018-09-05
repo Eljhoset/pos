1ff802e8da88feea9882673f901a489b21875165
eljhoset <jd-jd-@hotmail.com>
2018-09-05 08:22:27 -0400

    disable form authentication disable anonymous user denying all access allowing access to the token only for clients with 'ROLE_TRUSTED_CLIENT' authority change client and token store to jdbc

--- src/main/java/com/eljhoset/pos/security/config/SecurityConfig.java
+++ src/main/java/com/eljhoset/pos/security/config/SecurityConfig.java
@@ -3,12 +3,16 @@ package com.eljhoset.pos.security.config;
 import com.eljhoset.pos.user.model.jpa.Users;
 import com.eljhoset.pos.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -19,10 +23,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @author Daniel
  */
 @Configuration
+@EnableWebSecurity(debug = true)
+@Order(SecurityProperties.BASIC_AUTH_ORDER - 1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserService userService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .anonymous().disable()
+                .authorizeRequests().anyRequest().denyAll();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
